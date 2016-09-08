59371c81ddc44aa9d304c3d532b5a5a060a172a5
jiangyf <jiangyf@chetong.net>
2016-09-08 19:47:21 +0800

    war包启动

--- src/main/java/com/chetong/ctwechat/MainApplication.java
+++ src/main/java/com/chetong/ctwechat/MainApplication.java
@@ -25,7 +25,7 @@ public class MainApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
+		return application.sources(MainApplication.class);
 	}
 
 	@Bean
